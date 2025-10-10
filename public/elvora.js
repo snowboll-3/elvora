63346e39cc67b9700671aeca14e4b11fe4db447b
snowboll-3! <bvretenar@gmail.com>
2025-10-10 19:33:30 +0200

    Deploy Elvora web

---
+++ public/elvora.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("mousemove", (e)=>{
+  const x = (e.clientX / window.innerWidth  - .5) * 6;
+  const y = (e.clientY / window.innerHeight - .5) * 6;
+  document.getElementById("hero").style.backgroundPosition = `${50+x}% ${50+y}%`;
+});
